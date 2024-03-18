6af4e888e1751405bb3cfa5e499a9ced7e9e11c7
ramanagouda18 <157582075+ramanagouda18@users.noreply.github.com>
2024-03-18 16:50:19 +0530

    added video to index

--- index.js
+++ index.js
@@ -10,10 +10,28 @@ const {checkSchema} = require('express-validator')
 const userCtrl = require('./app/controllers/user-controller')
 const {userRegisterSchema, userLoginSchema} = require('./app/validations/userValidationSchema')
 const { authenticateUser, authorizeUser } = require('./app/middlewares/auth')
+const multer = require('multer')
 configureDB()
-
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        let uploadPath='./app/files/uploads/'
+        if (file.mimetype.startsWith('video')) {
+            uploadPath += 'videos/';
+        }else {
+            return cb(new Error('Invalid file type'));
+        }
+        cb(null,uploadPath)
+    },
+    filename:(req,file,cb)=>{
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+})
+const upload=multer({storage:storage})
 app.post('/api/register' ,checkSchema(userRegisterSchema),userCtrl.register )
 app.post('/api/login' , checkSchema(userLoginSchema),userCtrl.login)
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    res.send('File uploaded successfully');
+  });
 
 app.get('/api/vegetables' , authenticateUser , authorizeUser(['buyer']) , ()=>{
     console.log('all the vegetables')
