e914dfa2af09cd2d3e88607f6bcc79923cb72f4a
ramanagouda18 <157582075+ramanagouda18@users.noreply.github.com>
2024-03-18 15:36:12 +0530

    changes to product model

--- app/models/product-model.js
+++ app/models/product-model.js
@@ -1,6 +1,7 @@
 const {Schema , model } = require('mongoose')
 
 const productSchema = new Schema({
+    productCategory:String,
     productName:String ,
     productImg:String,
     productVideo:String,
